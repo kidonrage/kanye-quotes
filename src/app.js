92b1183f641672951d6e982fdb976d4e0c096d75
Vlad Eliseev <freshymanchan@gmail.com>
2020-04-05 17:57:34 +1000

    Random image selecting implemented

--- src/app.js
+++ src/app.js
@@ -4,3 +4,13 @@ console.log("Kanye west app works");
 
 const $background = document.getElementById('background-overlay');
 $background.style.backgroundImage = ''
+
+const lastImageIndex = 24;
+
+const imageIndex = randomIntFromInterval(0, lastImageIndex);
+const BGImageUrl = `url(images/${imageIndex}.jpg`
+$background.style.backgroundImage = BGImageUrl;
+
+function randomIntFromInterval(min, max) { // min and max included 
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HTMLPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const WriteFilePlugin = require('write-file-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
@@ -15,13 +17,25 @@ module.exports = {
     new HTMLPlugin({
       template: './src/index.html'
     }),
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),
+    new CopyPlugin([
+      { from: './src/images', to: 'images' },
+    ]),
+    new WriteFilePlugin({
+      test: /\.(png|svg|jpg|gif)$/,
+    })
   ],
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ],
       }
     ]
   }
